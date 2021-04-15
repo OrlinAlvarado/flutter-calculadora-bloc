e0c38feb968db0d75003375e3bb361db54d51967
Orlin Alvarado <oralvarado1@gmail.com>
2021-04-14 21:24:11 -0600

    Aplicacion terminada

---
+++ lib/bloc/calculator/calculator_bloc.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'calculator_event.dart';
+part 'calculator_state.dart';
+
+class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
+  CalculatorBloc() : super(CalculatorState());
+
+  @override
+  Stream<CalculatorState> mapEventToState(
+    CalculatorEvent event,
+  ) async* {
+    
+    //Borrar todo
+    if( event is ResetAC ){
+      yield* this._resetAC();
+    
+    //Agregar numeros  
+    } else if (event is AddNumber){
+      yield state.copyWith(
+        mathResult: state.mathResult == '0' 
+                      ? event.number 
+                      : state.mathResult + event.number,
+      );
+      //Signo negativo o positivo
+    } else if ( event is ChangeNegativePositive ){
+      yield state.copyWith(
+        mathResult: state.mathResult.contains('-') 
+                      ? state.mathResult.replaceFirst('-', '')
+                      : '-' + state.mathResult
+      );
+      //Eliminar ultimo caracter
+    } else if ( event is DeleteLastEntry ){
+      yield state.copyWith(
+        mathResult: state.mathResult.length > 1
+                      ? state.mathResult.substring(0, state.mathResult.length - 1)
+                      : '0'
+      );
+      
+    } else if ( event is OperationEntry ){
+      yield state.copyWith(
+        firstNumber: state.mathResult,
+        operation: event.operation,
+        mathResult: '0',
+        secondNumber: '0'
+      );
+    } else if ( event is CalculateResult ){
+      yield* _calculateResult();
+    }
+    
+    
+  }
+  
+  Stream<CalculatorState> _resetAC() async*{
+    yield CalculatorState(
+        firstNumber: '0',
+        mathResult: '0',
+        secondNumber: '0',
+        operation: '+'
+      );
+  }
+  
+  Stream<CalculatorState> _calculateResult() async*{
+    final double num1 = double.parse(state.firstNumber);
+    final double num2 = double.parse(state.mathResult);
+    
+    switch( state.operation ) {
+      case '+':
+        yield state.copyWith(
+          secondNumber: state.mathResult,
+          mathResult: '${ num1 + num2 }',
+        );
+      break;
+      case '-':
+        yield state.copyWith(
+          secondNumber: state.mathResult,
+          mathResult: '${ num1 - num2 }',
+        );
+      break;
+      case 'X':
+        yield state.copyWith(
+          secondNumber: state.mathResult,
+          mathResult: '${ num1 * num2 }',
+        );
+      break;
+      case '/':
+        if(num2 == 0){
+          yield state.copyWith(
+            firstNumber: '0',
+            secondNumber: '0',
+            mathResult: '0',
+          );
+        } else {
+          yield state.copyWith(
+            secondNumber: state.mathResult,
+            mathResult: '${ num1 / num2 }',
+          );
+        }
+      break;
+      
+      default:
+        yield state;
+    }
+  }
+}
